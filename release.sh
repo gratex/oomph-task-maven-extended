5a3832f4afacfee89e2fc75dc8cdba447fb484be
jkoval <jkoval@gratex.com>
2017-09-05 07:58:37 +0200

    release script typo

--- release.sh
+++ release.sh
@@ -27,7 +27,7 @@ git tag $VERSION
 git push --tags
 git push
 
-pushd $DIR/../oomph-task-maven-etended-gh-pages
+pushd $DIR/../oomph-task-maven-extended-gh-pages
 {
     git reset HEAD --hard
     git clean -dfx
